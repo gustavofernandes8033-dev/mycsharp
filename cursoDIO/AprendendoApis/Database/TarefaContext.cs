2ed6329b56626848cf0a3d3ee22aab64293f5b0c
gustavo <gustavo.fernandes.8033@gmail.com>
2026-03-22 19:42:36 -0300

    revertido o projeto para .net9. restore foi sucesso mas build apresentou erros. continuar o projeto na terça

--- cursoDIO/AprendendoApis/Database/TarefaContext.cs
+++ cursoDIO/AprendendoApis/Database/TarefaContext.cs
@@ -4,7 +4,7 @@ public class TarefasContext : DbContext
 {
 
     #nullable disable
-    public TarefasContext(DbContextOptions<TarefasContext> options) : base(options)
+    public TarefasContext(DbContextOptions<TarefasContext> options) : base(options);
     public DbSet<Tarefa> Tarefas {get;set;}
 
 }
