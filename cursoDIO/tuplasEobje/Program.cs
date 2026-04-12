9f1ee3cc9e6079a911530feb8fae7b91b2c4362e
Gustavo <gustavo.fernandes.8033@gmail.com>
2026-04-12 19:12:40 -0300

    adicionado um projeto sobre tuplas no c# e uma pasta anotacoes_c# onde se encontra txts, .mds e futuramente uma vault obsidian

---
+++ cursoDIO/tuplasEobje/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+(int identificacao, string nome, string sobrenome) tupla1 = (1, "helena", "fernandes");
+(int identificacao, string nome, string sobrenome) tupla2 = (2, "matheus", "liammmmmmmmmmm");
+Console.WriteLine(tupla2.nome);
