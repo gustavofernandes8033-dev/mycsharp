b233da230f705066c2c2f0a99ad760f633999474
Gustavo <gustavo.fernandes.8033@gmail.com>
2026-04-12 16:54:32 -0300

    desafio 6,7 e 8 concluidos. pasta exercicio POO 7 excluida.

---
+++ praticas/exercicio 7/Program.cs
@@ -0,0 +1,50 @@
+﻿//Abaixo segue um exemplo de código que você pode ou não utilizar
+using System;
+using System.Net.NetworkInformation;
+
+class minhaClasse {
+    
+
+    static void Main(string[] args) 
+    { 
+
+        //5
+        //2 5 4 20 10
+        int quantidadeDeNumerosDentroDaLista= int.Parse(Console.ReadLine());
+        string[] listaDeNumeros = Console.ReadLine().Split(' '); //que vai receber 5 numeros btw
+        int[] quantidadeMultiplos = new int[4];
+        // quantidadeMultiplos[0] = 2;
+        // quantidadeMultiplos[1] = 3;
+        // quantidadeMultiplos[2] = 4;
+        // quantidadeMultiplos[3] = 5;
+
+        for(int i = 0; i < quantidadeDeNumerosDentroDaLista; i++)
+        {         
+            if ((int.Parse(listaDeNumeros[i]) % 2  ) == 0)
+            {quantidadeMultiplos[0]++; }
+            if ((int.Parse(listaDeNumeros[i]) % 3  ) == 0)
+            {quantidadeMultiplos[1]++; }
+            if ((int.Parse(listaDeNumeros[i]) % 4  ) == 0)
+            {quantidadeMultiplos[2]++; }
+            if ((int.Parse(listaDeNumeros[i]) % 5  ) == 0)
+            {quantidadeMultiplos[3]++; }
+
+        }
+
+        for(int i = 0; i < 4; i++)
+        {
+            Console.WriteLine($"{quantidadeMultiplos[i]} Multiplo(s) de {i+2}");
+        }
+
+          
+     
+    }
+}
+
+
+
+
+
+
+
+/* 2 / 2 */
