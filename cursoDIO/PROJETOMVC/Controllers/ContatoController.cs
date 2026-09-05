35484340502160cce10b288793814e16e152b259
Gustavo <gustavo.fernandes.8033@gmail.com>
2026-09-05 13:23:39 -0300

    iplementado a pagina de Criar no projeto mvc. também iplementado método da pagina criar que é um httpPost.

--- cursoDIO/PROJETOMVC/Controllers/ContatoController.cs
+++ cursoDIO/PROJETOMVC/Controllers/ContatoController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PROJETOMVC.Context;
+using PROJETOMVC.Models;
 
 namespace PROJETOMVC.Controllers;
 
 public class ContatoController : Controller
 {
-  public IActionResult Index()
+
+  private readonly AgendaContext _context;
+
+  public ContatoController (AgendaContext context)
+  {
+    _context = context;
+  }
+
+public IActionResult Index()
+  {
+    var contatos = _context.Contatos.ToList();
+    return View(contatos);
+  }
+
+
+  //no mvc é opicional colocar "httpGet". por padrão, um IActionResult sem http vai ser um get."
+  public IActionResult Criar()
   {
     return View();
+  }
+
+  [HttpPost]
+  public IActionResult Criar(Contato contato)
+  {
+    if (ModelState.IsValid)
+    {
+      _context.Contatos.Add(contato);
+      _context.SaveChanges();
+      return RedirectToAction(nameof(Index));
+    }
+    return View(contato);
   }
 
 }
